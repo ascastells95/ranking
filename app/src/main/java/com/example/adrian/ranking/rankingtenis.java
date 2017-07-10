9d1c6b4863590e2d52bff3e9a9b23a0fabc8c732
ascastells95 <ascastells95@gmail.com>
2017-07-10 18:20:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/adrian/ranking/rankingtenis.java
@@ -0,0 +1,13 @@
+package com.example.adrian.ranking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class rankingtenis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rankingtenis);
+    }
+}
